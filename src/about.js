194209da87dfc710f520f16151abcb1da1099d1c
Eoin-k <100050380+Eoin-k@users.noreply.github.com01~>
2024-07-08 14:45:37 +0100

    Added dist to use for GH pages

---
+++ src/about.js
@@ -0,0 +1,7 @@
+function aboutPage () {
+    content.innerHTML = "";
+    content.innerHTML = `<div class="meme">Really entertaining story</div>
+    <div class="meme">Some drivel about farm to fork, exceptional ingredients from some catring supply company</div>`
+}
+
+export default aboutPage;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import  './style.css';
 import menuPage from './menu'
+import aboutPage from './about';
 const menu = document.getElementById("menu")
 const home = document.getElementById("home");
 const about = document.getElementById("about")
 function mainContent(){
      const content = document.getElementById("content");
-     const mainPage = `<div class="meme">HI there</div>`;
      content.innerHTML = "";
      content.innerHTML += `<div class="meme">HI there</div>`;
 }
@@ -13,4 +13,4 @@ function mainContent(){
 mainContent();
 menu.addEventListener("click", menuPage);
 home.addEventListener("click", mainContent);
-about.addEventListener("click",(c));
+about.addEventListener("click",aboutPage);
