84e8024c293d629bb09031f12db210f578a52540
Eoin-k <100050380+Eoin-k@users.noreply.github.com01~>
2024-07-09 17:16:18 +0100

    Final styling

--- src/about.js
+++ src/about.js
@@ -1,7 +1,12 @@
 function aboutPage () {
     content.innerHTML = "";
-    content.innerHTML = `<div class="about-intro">Really entertaining story</div>
-    <div class="about-body">Some drivel about farm to fork, exceptional ingredients from some catring supply company</div>`
+    content.innerHTML = `<div><h1 class="about-header">About Epicurian Delights</h1></div>
+    <div class="about-body about-container">
+    <p>At Epicurean Delights, we take pride in our ability to fuse the rich traditions of classic cuisine with the creativity and flair of modern culinary artistry. Our chefs are passionate about honoring the essence of traditional dishes while reinterpreting them through innovative techniques and presentations. Each ingredient is meticulously sourced to ensure the highest quality and freshness, allowing us to craft dishes that are not only visually stunning but also bursting with flavor.</p>
+    <p>Our menu is a testament to our dedication, featuring a diverse array of options that cater to a variety of tastes and dietary preferences. Whether you are indulging in our signature modern twist on a beloved classic or exploring a new culinary creation, every meal at Epicurean Delights is designed to be a memorable experience that excites and delights the senses.</p>
+    <p>Beyond our exceptional cuisine, Epicurean Delights is committed to providing an unparalleled dining experience through our elegant ambiance and impeccable service. From the moment you step through our doors, you will be greeted by a warm and inviting atmosphere that perfectly complements the sophistication of our dishes. Our attentive and knowledgeable staff are dedicated to ensuring that your visit is nothing short of extraordinary, offering personalized recommendations and accommodating any special requests with grace and professionalism. </p>
+    <p>Whether you are celebrating a special occasion, hosting a business dinner, or simply enjoying a night out, Epicurean Delights offers the perfect setting for any event. Join us and discover why our restaurant is renowned not only for its exquisite cuisine but also for its unwavering commitment to excellence in every aspect of the dining experience.</p>
+    </div>`
     
 }
 
